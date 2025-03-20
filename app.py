cddae2cbf516b3876eee3c728b8c3f8bfdfbdb20
f-de-lima <f.de-lima-miguel@tbs-education.org>
2025-03-20 18:35:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,6 @@ uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
 
 if uploaded_file:
   df = pd.read_csv(uploaded_file)
-  st.write("Here is the preview of the Uploaded CSV: ")
-  st.dataframe(df.head())
-
-# Bar chart of customers by country
-  st.subheader("Customers by City")
-  country_counts = df['customer_city'].value_counts()
-  #st.write(country_counts)
-  st.bar_chart(country_counts)
 
 # getting info
   cars_brand = st.sidebar.multiselect("Select the car brand", df["car_brand"].unique(),  df["car_brand"].unique())
@@ -66,62 +58,6 @@ if uploaded_file:
   Trips_Count = df["Trips Date"].value_counts()
   st.line_chart(Trips_Count)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#st.title("User CSV File Viewer")
-
-# Request the user to Upload the File 
-# uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
-
-# if uploaded_file:
-#     df = pd.read_csv(uploaded_file)
-#     st.write("Here is the preview of the Uploaded CSV: ")
-#     st.dataframe(df.head())
-
-#     # Chart 1: Bar chart of customers by country
-#     st.subheader("Customers by City")
-#     country_counts = df['customer_city'].value_counts()
-#     st.write(country_counts)
-#     st.bar_chart(country_counts)
-
-#     # Chart 2: Line chart of subscriptions over time
-#     st.subheader("Trips Over Time")
-#     df['Trip Date'] = pd.to_datetime(df['pickup_time']).dt.date
-#     subscriptions_by_date = df.groupby('Trip Date').size().reset_index(name='Count')
-#     st.line_chart(subscriptions_by_date.set_index('Trip Date'))
-
-
-
+  st.write("Here is the preview of the Uploaded CSV: ")
+  st.dataframe(df.head())
 
