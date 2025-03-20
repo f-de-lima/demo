4efc3a34542b30851c1d6a96eaf5a67c0433581e
f-de-lima <f.de-lima-miguel@tbs-education.org>
2025-03-20 17:29:25 +0000

    comments in code

--- app.py
+++ app.py
@@ -11,54 +11,57 @@ if uploaded_file:
   st.write("Here is the preview of the Uploaded CSV: ")
   st.dataframe(df.head())
 
-# Chart 1: Bar chart of customers by country
+# Bar chart of customers by country
   st.subheader("Customers by City")
   country_counts = df['customer_city'].value_counts()
   #st.write(country_counts)
   st.bar_chart(country_counts)
 
+# getting info
   cars_brand = st.sidebar.multiselect("Select the car brand", df["car_brand"].unique(),  df["car_brand"].unique())
   df = df[df["car_brand"].isin(cars_brand)]
-
+# setting cols to show numbers
   col1, col2, col3, col4  = st.columns(4)
-
+# col1 = number of trips
   col1.metric("Number of Trips", df.shape[0], border=True)
+# col2 = unique customers
   col2.metric("Unique Customers",  df["customer_email"].nunique(),border=True)
+# col3 = calculating total distance
   with col3:
     total_distance = df['distance'].sum() / 1000
     st.metric("Total Distance", value=f"{total_distance:.2f} K", border=True)
+# col4 calculating average revenue per trip
   with col4:
     average_revenue = df['revenue'].mean()
     st.metric("Average Revenue Per Trip", value=f"{average_revenue:.2f} €", border=True)
 
-
-
+# setting new cols for plotting
   col1, col2, col3 = st.columns(3)
-  # Chart 1: Bar chart of customers by country
+# col1 = customers by country
   with col1:
     st.subheader("Customers by City")
     country_counts = df['customer_city'].value_counts()
     st.bar_chart(country_counts)
-
-  # Chart 2 : Revenue by Car Model
+# col2 = Revenue by Car Model
   with col2:
     st.subheader("Revenue by Car Model")
     revenue_by_car = df.groupby('car_model')['revenue'].sum()
     st.bar_chart(revenue_by_car)
-  # Chart 3 : Average Trip distance per city
+# col3 = Average Trip distance per city
   with col3:
     st.subheader("Average Trip Distance per city")
     avg_distance_by_city = df.groupby('customer_city')['distance'].mean()
     st.bar_chart(avg_distance_by_city)
-  # Convert the pickup time to a date type column 
+
+# Convert the pickup time to a date type column 
   df['Trips Date'] = pd.to_datetime(df['pickup_time']).dt.date
 
-  # Chart 3: Revenue over time 
+# Revenue over time 
   st.subheader("Revenue Over Time")
   revenue_over_time = df.groupby('Trips Date')['revenue'].sum()
   st.area_chart(revenue_over_time)
 
-  # Chart 4: Line chart of Trips over time
+# Line chart of Trips over time
   st.subheader("Trips Over Time")
   Trips_Count = df["Trips Date"].value_counts()
   st.line_chart(Trips_Count)
